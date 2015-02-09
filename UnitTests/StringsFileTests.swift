61ca6932e8ea19f29ec227304228fdbb1dc4b6ac
Yosuke Ishikawa <y@ishkawa.org>
2015-02-09 22:55:14 +0900

    enumerate .strings

--- UnitTests/StringsFileTests.swift
+++ UnitTests/StringsFileTests.swift
@@ -1,16 +1,24 @@
 import Cocoa
 import XCTest
 
+private let projectPath = NSFileManager.defaultManager().currentDirectoryPath
+private let fixturePath = projectPath.stringByAppendingPathComponent("UnitTests/Fixture.strings")
+private let projectURL = NSURL(fileURLWithPath: projectPath)!
+private let fixtureURL = NSURL(fileURLWithPath: fixturePath)!
+
 class StringsFileTests: XCTestCase {
-    func testLoadingStrings() {
-        let projectPath = NSFileManager.defaultManager().currentDirectoryPath
-        let fixturePath = projectPath.stringByAppendingPathComponent("UnitTests/Fixture.strings")
-        let fixtureURL = NSURL(fileURLWithPath: fixturePath)!
-        let strings = StringsFile(URL: fixtureURL)!
-        
-        XCTAssertEqual(strings.dictionary, [
+    func testInitWithURL() {
+        let stringsFile = StringsFile(URL: fixtureURL)!
+        XCTAssertEqual(stringsFile.dictionary, [
             "qCi-L7-NSA.normalTitle": "Button",
             "veg-fi-Qyb.text": "Hello",
         ])
+    }
+    
+    func testEnumerateFilesUnderDirectory() {
+        let files = StringsFile.stringsFilesInDirectory(projectURL)
+        let stringsFile = StringsFile(URL: fixtureURL)!
+        XCTAssertEqual(countElements(files), 1)
+        XCTAssertEqual(first(files)!.URL, stringsFile.URL)
     }
 }
--- ls2xs/StringsFile.swift
+++ ls2xs/StringsFile.swift
@@ -4,14 +4,40 @@ class StringsFile {
     let URL: NSURL
     let dictionary: [String: String]
     
+    class func stringsFilesInDirectory(directoryURL: NSURL) -> [StringsFile] {
+        var files = [StringsFile]()
+        
+        let fileManager = NSFileManager.defaultManager()
+        let enumerator = fileManager.enumeratorAtURL(directoryURL, includingPropertiesForKeys: [], options: .SkipsHiddenFiles) { URL, error in
+            if let error = error {
+                println("error: \(error)")
+                return false
+            }
+            return true
+        }
+        
+        while let URL = enumerator?.nextObject() as? NSURL {
+            if let file = StringsFile(URL: URL) {
+                files.append(file)
+            }
+        }
+        
+        return files
+    }
+    
     init?(URL: NSURL) {
+        // In Swift 1.1, stored properties must be initialized (radar://18216578).
+        self.URL = NSURL()
+        self.dictionary = [String: String]()
+        
+        if URL.pathExtension? != "strings" {
+            return nil
+        }
+    
         if let dictionary = NSDictionary(contentsOfURL: URL) as? [String: String] {
             self.URL = URL
             self.dictionary = dictionary
         } else {
-            // In Swift 1.1, stored properties must be initialized (radar://18216578).
-            self.URL = NSURL()
-            self.dictionary = [String: String]()
             return nil
         }
     }
