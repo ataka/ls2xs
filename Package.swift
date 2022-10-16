bf0c302c8c3c11f494768b95e00882ad70a887c8
Masayuki Ataka <masayuki.ataka@gmail.com>
2022-10-16 18:45:12 +0900

    Update swift-argument-parser library from 0.2.0 to 1.1.4

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .executable(name: "ls2xs", targets: ["ls2xs"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.2.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.1.0"),
     ],
     targets: [
         .executableTarget(
